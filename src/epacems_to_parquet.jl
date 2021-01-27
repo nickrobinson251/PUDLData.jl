96fb86cf4b3decceb4ea437ed6f8113bc2429589
Nick Robinson <nicholas.robinson@invenialabs.co.uk>
2021-01-27 15:17:27 +0000

    Remove accidentally added file

--- src/epacems_to_parquet.jl
+++
@@ -1,3 +0,0 @@
-const CEMS_STATES = ["AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM",  "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"];
-
-const CEMS_YEARS = Dates.year.(Date(1995):Year(1):Date(2018))
